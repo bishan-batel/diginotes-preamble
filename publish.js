8e99cff81f806d26ce6a632b66165bdc8a8acc3b
Kishan S Patel <bishan.batel@protonmail.com>
2023-11-29 15:28:37 -0800

    initial

--- publish.js
+++ publish.js
@@ -1,11 +1,16 @@
-console.log("jax moment");
-
 (async function loadPreamble() {
-  if (MathJax) {
+  if (window.MathJax && window.MathJax.tex2chtml) {
     const preambleURL =
       "https://raw.githubusercontent.com/bishan-batel/diginotes-preamble/main/preamble.sty";
-    MathJax.tex2chtml(await (await fetch(preambleURL)).text());
-  }
 
-  setTimeout(loadPreamble, 100);
+    console.log("Loading preamble");
+    MathJax.texReset();
+    MathJax.typesetClear();
+    MathJax.tex2chtml(await (await fetch(preambleURL)).text());
+    MathJax.typesetPromise();
+    console.log("Preamble Loaded!");
+
+    return;
+  }
+  setTimeout(loadPreamble, 5);
 })();
